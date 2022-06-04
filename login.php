cb5ef428072b354f352e640fd1ec9d06deed1e70
Anamaria Camarasan <acamaras@student.hive.fi>
2022-06-04 19:25:40 +0300

    login page html created

--- login.php
+++ login.php
@@ -1,3 +1,31 @@
 <?php
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <img src="img/cam.png" alt="camera" class="camera1">
+    <h1>Login</h1>
+
+    <div class="username">
+        Username:
+        <input></input>
+    </div>
+    <div class="password">
+        Password:
+        <input></input>
+    </div>
+
+    <div class="footer">
+    <?php	include('partials/footer.php');	?>
+    </div>
+</body>
+</html>
