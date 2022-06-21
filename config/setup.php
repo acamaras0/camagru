1b06a1af23adfca922407ff32381c1fe6252144a
Anamaria Camarasan <acamaras@student.hive.fi>
2022-06-21 17:52:58 +0300

    tables for likes and comments created

--- config/setup.php
+++ config/setup.php
@@ -63,9 +63,47 @@ catch(PDOException $e)
 $conn = null;
 
 //create user_likes table
+try
+{
+    $conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE IF NOT EXISTS `user_likes`(
+        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        created_at timestamp not null default current_timestamp(),
+        picture_name TEXT NOT NULL,
+        picture_owner VARCHAR(50) NOT NULL,
+        id_owner  INT(11) NOT NULL
+        )";
+    $conn->exec($sql);
 
-
+}
+catch(PDOException $e)
+{
+    echo $sql . "<br>" . $e->getMessage();
+}
+$conn = null;
 
 //create user_comments table
+try
+{
+    $conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE IF NOT EXISTS `user_comments`(
+        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        comments VARCHAR(500) NOT NULL,
+        created_at timestamp not null default current_timestamp(),
+        picture_name TEXT NOT NULL,
+        picture_owner VARCHAR(50) NOT NULL,
+        id_owner  INT(11) NOT NULL
+        )";
+    $conn->exec($sql);
+
+}
+catch(PDOException $e)
+{
+    echo $sql . "<br>" . $e->getMessage();
+}
+$conn = null;
+
 
 ?>
