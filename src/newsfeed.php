3aa8d1041115629fef982b4cbbe5e45f3ccfc17e
Anamaria Camarasan <acamaras@student.hive.fi>
2022-06-23 14:21:04 +0300

    added pagination to the newsfeed

--- src/newsfeed.php
+++ src/newsfeed.php
@@ -7,10 +7,24 @@
         header("Location: ../index.php");
     get_id();
     $conn = connection();
-    $sql = "SELECT * FROM `user_pictures` ORDER BY created_at DESC";
+    if (isset($_GET['page_no']) && $_GET['page_no'] != "")
+    {
+        $page = $_GET['page_no'];
+    }
+    else
+    {
+        $page = 1;
+    }
+    $total_pictures_per_page = 5;
+    $pictures = ($page - 1) * $total_pictures_per_page;
+    $next_page = $page + 1;
+    $prev_page = $page - 1;
+    $sql = "SELECT COUNT(*) FROM user_pictures"; 
     $qry = $conn->query($sql);
-    $res = $qry->fetchAll(PDO::FETCH_ASSOC);?>
-
+    $res = $qry->fetchAll(PDO::FETCH_ASSOC);
+    $total_pictures = $res[0]['COUNT(*)'];
+    $total_pages = ceil($total_pictures / $total_pictures_per_page);
+?>
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -28,36 +42,53 @@
                 <?php include('../partials/header_newsfeed.php'); ?>
             </div>
     <?php
-    if ($res)
+    try
     {
-        foreach($res as $key)
+     $conn = connection();
+     $sql0 = "SELECT * FROM `user_pictures` ORDER BY id DESC LIMIT $pictures, $total_pictures_per_page";
+     $qry0 = $conn->query($sql0);
+     $res0 = $qry0->fetchAll(PDO::FETCH_ASSOC);
+        if($res0)
         {
-            $id = $key['picture_name'];
-            ?>
-                <div class="middle-profile">
-                    <div class="border-profile">
-                        <?php
-                        if ($_SESSION['logged_user_id'] == $key['id_owner'])
-                        {
-                         ?>
-                         <form action="delete_pic.php" method="post">
-                            <button class="delete" type="submit" name="delete_pic" value="Delete"> <img src="../img/delete.png" width="18" alt="del"></button>
-                            <input type="hidden" name="picture_path" value=<?php echo $key['picture_path'];?>>
-                        </form>
-                        <?php
-                        }
-                        ?>
-                        <div class="username"><?php echo "@" . $key['picture_owner'];?></div>
-                        <?php echo " " . $key['created_at']?>
-                        <img class="picture" src=<?php echo $key['picture_path'];?>>
+            foreach($res0 as $key)
+            {
+                $picture_id = $key['picture_name'];
+                ?>
+                    <div class="middle-profile">
+                        <div class="border-profile">
+                            <?php
+                            if ($_SESSION['logged_user_id'] == $key['id_owner'])
+                            {
+                            ?>
+                            <form action="delete_pic.php" method="post">
+                                <button class="delete" type="submit" name="delete_pic" value="Delete"> <img src="../img/delete.png" width="18" alt="del"></button>
+                                <input type="hidden" name="picture_path" value=<?php echo $key['picture_path'];?>>
+                            </form>
+                            <?php
+                            }
+                            ?>
+                            <div class="username"><?php echo "@" . $key['picture_owner'];?></div>
+                            <?php echo " " . $key['created_at']?>
+                            <img class="picture" src=<?php echo $key['picture_path'];?>>
+                        </div>
                     </div>
-                </div>
-        <?php
+            <?php
+            }
         }
     }
+    catch(PDOException $e)
+    {
+        echo $qry0 . "<br>" . $e->getMessage();
+    }
+    $conn = null;
 ?>
-            <div class="footer">
-                <?php	include('../partials/footer.php');	?>
-            </div>
-        </body>
-        </html> 
+    <div class="pagination">
+            <a class="arrows" <?php if($page > 1){echo "href='?page_no=$prev_page'";} ?>> ⫷⫷⫷ </a>&nbsp&nbsp&nbsp&nbsp&nbsp
+            <?php echo $page; ?>&nbsp&nbsp&nbsp&nbsp&nbsp
+            <a class="arrows" <?php if($page < $total_pages){echo "href='?page_no=$next_page'";} ?>> ⫸⫸⫸ </a>
+    </div>
+        <div class="footer">
+            <?php	include('../partials/footer.php');	?>
+        </div>
+    </body>
+    </html> 
