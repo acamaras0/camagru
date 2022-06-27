5c9d2343827ae024656205283ec56076a1d1b7be
anama <anama@DESKTOP-NC3SKCA.(none)>
2022-06-27 18:51:10 +0300

    fixed some warnings

--- src/create.php
+++ src/create.php
@@ -6,17 +6,18 @@
     require_once('print_msg.php');
     session_start();
 
-    $new_email = $_POST['email'];
-    $new_fullname = $_POST['name'];
-    $new_user = strip_tags($_POST['login']);
-    $new_pwd = $_POST['passwd'];
-    $re_pwd = $_POST['re-passwd'];
-    $status = 0;
-    $notifications = 1;
-    $activation_code = md5($new_email.time());
+
 
     if(isset($_POST['submit']))
     {
+        $new_email = $_POST['email'];
+        $new_fullname = $_POST['name'];
+        $new_user = strip_tags($_POST['login']);
+        $new_pwd = $_POST['passwd'];
+        $re_pwd = $_POST['re-passwd'];
+        $status = 0;
+        $notifications = 1;
+        $activation_code = md5($new_email.time());
         if(strlen($new_user) > 20 || strlen($new_user) < 4)
         {
             print_msg("Username has to be in between 4 and 20 characters long.");
@@ -32,47 +33,48 @@
             print_msg("Passwords have to be identical, minimum 10 characters long, including a number, a capital letter and a special character.");
             header('Refresh: 3; create.php');
         }
-    }
+    
 
-    if($_POST['email'] && $_POST['name'] && $_POST['login'] && $_POST['passwd'] === $_POST['re-passwd'] && isset($_POST['submit']))
-    {
-        $double_user_verification = verif_user($new_email, $new_user);
-        if ($double_user_verification == 0)
+        if($_POST['email'] && $_POST['name'] && $_POST['login'] && $_POST['passwd'] === $_POST['re-passwd'] && isset($_POST['submit']))
         {
-            $new_pwd = hash('whirlpool', $new_pwd);
-            try
+            $double_user_verification = verif_user($new_email, $new_user);
+            if ($double_user_verification == 0)
             {
-                $conn = connection();
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $stm = $conn->prepare("INSERT INTO user_info (email, fullname, u_name, pwd, activation_code, activ_status, notif_status)
-                                    VALUES (:new_email, :new_fullname, :new_user, :new_pwd, :activation_code, :activ_status, :notif_status)");
-                $stm->bindParam(':new_email', $new_email, PDO::PARAM_STR);
-                $stm->bindParam(':new_fullname', $new_fullname, PDO::PARAM_STR);
-                $stm->bindParam(':new_user', $new_user, PDO::PARAM_STR);
-                $stm->bindParam(':new_pwd', $new_pwd, PDO::PARAM_STR);
-                $stm->bindParam(':activation_code', $activation_code, PDO::PARAM_STR);
-                $stm->bindParam(':activ_status', $status, PDO::PARAM_STR);
-                $stm->bindParam(':notif_status', $notifications, PDO::PARAM_STR);
-                $stm->execute();
+                $new_pwd = hash('whirlpool', $new_pwd);
+                try
+                {
+                    $conn = connection();
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $stm = $conn->prepare("INSERT INTO user_info (email, fullname, u_name, pwd, activation_code, activ_status, notif_status)
+                                        VALUES (:new_email, :new_fullname, :new_user, :new_pwd, :activation_code, :activ_status, :notif_status)");
+                    $stm->bindParam(':new_email', $new_email, PDO::PARAM_STR);
+                    $stm->bindParam(':new_fullname', $new_fullname, PDO::PARAM_STR);
+                    $stm->bindParam(':new_user', $new_user, PDO::PARAM_STR);
+                    $stm->bindParam(':new_pwd', $new_pwd, PDO::PARAM_STR);
+                    $stm->bindParam(':activation_code', $activation_code, PDO::PARAM_STR);
+                    $stm->bindParam(':activ_status', $status, PDO::PARAM_STR);
+                    $stm->bindParam(':notif_status', $notifications, PDO::PARAM_STR);
+                    $stm->execute();
+                }
+                catch(PDOException $e)
+                {
+                    echo $stm . "<br>" . $e->getMessage();
+                }
+                $conn = null;
+                send_email($new_email, $activation_code, $new_user, $new_pwd, 1);
+                print_msg("User created succesfully! Activation link sent!");
+                header("Refresh: 2; login.php?message=1");
             }
-            catch(PDOException $e)
+            else if($double_user_verification == 2)
             {
-                echo $stm . "<br>" . $e->getMessage();
+                print_msg("Email address already in use.");
+                header("Refresh: 2; create.php?message=2");
             }
-            $conn = null;
-            send_email($new_email, $activation_code, $new_user, $new_pwd, 1);
-            print_msg("User created succesfully! Activation link sent!");
-            header("Refresh: 2; login.php?message=1");
-        }
-        else if($double_user_verification == 2)
-        {
-            print_msg("Email address already in use.");
-            header("Refresh: 2; create.php?message=2");
-        }
-        else if($double_user_verification == 1)
-        {
-            print_msg("Username already in use.");
-            header("Refresh: 2; create.php?message=3");
+            else if($double_user_verification == 1)
+            {
+                print_msg("Username already in use.");
+                header("Refresh: 2; create.php?message=3");
+            }
         }
     }
 ?>
--- src/upload.php
+++ src/upload.php
@@ -38,7 +38,7 @@
         }
         
         // Check file size
-        if ($_FILES["fileToUpload"]["size"] > 1000000) 
+        if ($_FILES["fileToUpload"]["size"] > 100000000) 
         {
             print_msg ("Sorry, your file is too large.");
             $uploadOk = 0;
